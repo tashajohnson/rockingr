f220d82651ecefe403e7fc57cc31a9fb0cd6c5a9
tashajohnson <tasha.hubner@gmail.com>
2016-12-01 15:58:44 -0700

    footer links updated

--- modules/components/Footer.js
+++ modules/components/Footer.js
@@ -9,19 +9,19 @@ class Footer extends React.Component {
                 <div className="col-lg-12">
                     <ul className="list-inline">
                         <li>
-                            <a href="#">Home</a>
+                            <a href="/">Home</a>
                         </li>
                         <li className="footer-menu-divider">&sdot;</li>
                         <li>
-                            <a href="#about">About</a>
+                            <a href="/about">About</a>
                         </li>
                         <li className="footer-menu-divider">&sdot;</li>
                         <li>
-                            <a href="#services">Products</a>
+                            <a href="/ordering">Ordering</a>
                         </li>
                         <li className="footer-menu-divider">&sdot;</li>
                         <li>
-                            <a href="#contact">Contact</a>
+                            <a href="/contact">Contact</a>
                         </li>
                     </ul>
                     <p className="copyright text-muted small">Copyright &copy; Rocking R Farms 2016. All Rights Reserved</p>
